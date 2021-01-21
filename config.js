72afbbfc9025b32dd12be1120b49e5112d7ccb4b
George Melek <melekgeorge@outlook.com>
2021-01-21 14:53:22 -0500

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports ={
+    MONGODB:
+        'mongodb+srv://georgemelek:8412sony1234@cluster0.iturt.mongodb.net/merng?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
